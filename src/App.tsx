459015a1c21a68e5d6a0b8ff5f11b491a39cb51e
Manas Ranjan Adha <manas865873@gmail.com>
2024-09-19 20:29:27 +0530

    setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex justify-center items-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +16,21 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-4xl font-bold mb-4">Vite + React</h1>
+      <div className="flex flex-col items-center gap-4">
+        <button 
+        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="text-lg">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-lg">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
